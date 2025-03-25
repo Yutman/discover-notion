dba73786ab18a55737dbcda7d94cd28962fcfe5f
Yutman <kuriapeter95@gmail.com>
2025-03-26 00:17:22 +0300

    Add reorderNodes function for handling node reordering with arrayMove

--- src/state/usePageState.tsx
+++ src/state/usePageState.tsx
@@ -1,5 +1,6 @@
 import { Page, NodeData, NodeType } from '../utils/types';
 import {useImmer} from 'use-immer';
+import { arrayMove } from '@dnd-kit/sortable';
 
 
 export const usePageState = (initialState: Page) => {
@@ -48,6 +49,14 @@ export const usePageState = (initialState: Page) => {
         });
     }; // method to update the cover image of the page
 
+    const reorderNodes = (id1: string, id2: string) => {
+        setPage((draft) => {
+            const index1 = draft.nodes.findIndex((node) => node.id === id1);
+            const index2 = draft.nodes.findIndex((node) => node.id === id2);
+            draft.nodes = arrayMove(draft.nodes, index1, index2);
+        })
+    }
+
     return {
         nodes: page.nodes,
         title: page.title,
@@ -59,6 +68,6 @@ export const usePageState = (initialState: Page) => {
         setTitle,
         setCoverImage,
         setNodes,
-    }
-
+        reorderNodes
+    };
 }

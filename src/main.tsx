821cc71a1c1fadc371ec659505cc0a070eed95a9
Yutman <kuriapeter95@gmail.com>
2025-03-26 23:30:10 +0300

    Create authsession context

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { AuthSessionProvider } from "./auth/AuthSessionContext";
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+    <AuthSessionProvider>
       <App />
+    </AuthSessionProvider>
     </BrowserRouter>
   </StrictMode>
 );
